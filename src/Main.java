9e5fa7f900ae941b55f8e59b62d8d52f8affb6f7
Grebell <100739501+Grebell@users.noreply.github.com>
2023-09-12 00:34:09 -0400

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,9 @@
 
 import java.io.File;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author Vinyl
+ * @author Grebell
  */
 public class Main {
 
